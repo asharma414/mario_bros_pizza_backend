6e1f3aa2ef586a126437d0abb816d22129192758
asharma414 <abhi.sharm.414@gmail.com>
2020-06-12 18:04:27 -0400

    login route

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -10,4 +10,14 @@ class CustomersController < ApplicationController
     def show
     end
 
+    def login
+        customer = Customer.find_by(username: params[:username])
+        if customer
+            session[:username] = params[:username]
+            render json: customer
+        else
+            render json:  { error: 'This username is invalid' }
+        end
+    end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   resources :ingredients, only: [:index]
   resources :pizzas, only: [:create, :index]
   resources :customers, only: [:create, :update, :show]
+  post '/login', to: 'customers#login'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
