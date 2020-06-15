67bc953dd27a0614403bb69e23d4815941a89f3c
asharma414 <abhi.sharm.414@gmail.com>
2020-06-15 13:03:31 -0400

    order POST handling fail

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,9 +1,22 @@
 class OrdersController < ApplicationController
 
     def create
-        ingreds = params[:ingredients].map {|id| Ingredient.find(id)}
-        pizza = Pizza.find_or_create_by(size: params[:size], bake: params[:bake], cut: params[:cut], ingredients: ingreds)
-        render json: Order.create(customer_id: params[:customer_id], pizza_id: pizza.id, delivery_instructions: params[:delivery_instructions])
+        ingreds = params[:ingredients].map {|id| Ingredient.find(id)}.sort_by {|ing| ing.name }
+        similarPizzas = Pizza.all.select {|pizza| pizza.ingredients.sort_by {|ing| ing.name } == ingreds}
+        if similarPizzas.empty?
+            pizza = Pizza.create(size: params[:size], bake: params[:bake], cut: params[:cut])
+            pizza.ingredients = ingreds
+            render json: Order.create(customer_id: params[:customer_id], pizza_id: pizza.id, delivery_instructions: params[:delivery_instructions])
+        else
+            similarCuts = similarPizzas.select {|pizza| pizza.size == params[:size] && pizza.bake == params[:bake] && pizza.cut == params[:cut]}
+            if similarCuts.empty?
+                pizza = Pizza.create(size: params[:size], bake: params[:bake], cut: params[:cut])
+                pizza.ingredients = ingreds
+                render json: Order.create(customer_id: params[:customer_id], pizza_id: pizza.id, delivery_instructions: params[:delivery_instructions])
+            else
+                render json: Order.create(customer_id: params[:customer_id], pizza_id: similarCuts[0], delivery_instructions: params[:delivery_instructions])
+            end
+        end
     end
 
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,5 @@
 class Order < ApplicationRecord
     belongs_to :customer
     belongs_to :pizza
+    # accepts_nested_attributes_for :pizza
 end
